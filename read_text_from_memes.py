7ec527994f7c50da467b156a0e5906724048b749
Kacper Boczkowski <kacper.boczkowski45@gmail.com>
2020-06-02 21:42:48 +0200

    Create dictionary of words in every meme

--- read_text_from_memes.py
+++ read_text_from_memes.py
@@ -12,19 +12,20 @@ def main():
 
 def analyze_memes_in_dir(directory):
     memes = os.scandir(directory)
+    names_and_words = dict()
     for file_name in memes:
         full_path = directory + "/" + file_name.name
-        print(full_path)
+        print("Filename: %s" %(full_path))
         meme_text = read_text_from_path(full_path)
-        analyze_string(meme_text)
+        analyze_string(meme_text, full_path, names_and_words)
 
-def analyze_string(meme_string):
+
+def analyze_string(meme_string, full_path, dictionary):
     for line in meme_string.splitlines():
         line = re.sub("[^A-Za-z]+", " ", line) # remove every sign other than A-z
-        line = re.sub("  ", "", line)
-        line = re.sub("\n", "", line)
         line = re.sub(r'\b\w{1,3}\b', '', line) # remove every word of length 3 or shorter
-        print(line)
+        if len(line) != 0 and not line.isspace():
+            dictionary[full_path] = line.split()
 
 def read_text_from_path(path):
     image = cv2.imread(path)
