1631c73ffa45aacacd5803b2873aa1042dfbbed1
Kacper Boczkowski <kacper.boczkowski45@gmail.com>
2020-06-03 21:34:41 +0200

    Add dictionary of memes path and words longer than 3, that they contain.

--- read_text_from_memes.py
+++ read_text_from_memes.py
@@ -2,23 +2,28 @@ import os
 import re
 import cv2
 import pytesseract
+import pickle
 
 DANK_MEMES_DIR = 'dankmemes-checked'
 REGULAR_MEMES_DIR = 'regularmemes-checked'
 ALT_MEMES_DIR = 'newalternative-checked'
 
 def main():
-    analyze_memes_in_dir(DANK_MEMES_DIR)
+    names_words_dict = dict()
+    analyze_memes_in_dir(DANK_MEMES_DIR, names_words_dict)
+    analyze_memes_in_dir(REGULAR_MEMES_DIR, names_words_dict)
+    analyze_memes_in_dir(ALT_MEMES_DIR, names_words_dict)
+    file_handle = open("names_words_dict.pkl", "wb")
+    pickle.dump(names_words_dict, file_handle)
+    file_handle.close()
 
-def analyze_memes_in_dir(directory):
+def analyze_memes_in_dir(directory, names_words_dict):
     memes = os.scandir(directory)
-    names_and_words = dict()
     for file_name in memes:
         full_path = directory + "/" + file_name.name
         print("Filename: %s" %(full_path))
         meme_text = read_text_from_path(full_path)
-        analyze_string(meme_text, full_path, names_and_words)
-
+        analyze_string(meme_text, full_path, names_words_dict)
 
 def analyze_string(meme_string, full_path, dictionary):
     for line in meme_string.splitlines():
@@ -26,6 +31,8 @@ def analyze_string(meme_string, full_path, dictionary):
         line = re.sub(r'\b\w{1,3}\b', '', line) # remove every word of length 3 or shorter
         if len(line) != 0 and not line.isspace():
             dictionary[full_path] = line.split()
+    if not full_path in dictionary:
+        dictionary[full_path] = ""
 
 def read_text_from_path(path):
     image = cv2.imread(path)
